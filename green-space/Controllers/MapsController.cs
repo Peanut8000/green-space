617ef9ba592bf75d951943705df524db7e15f15c
Peanut8000 <peanut60218@gmail.com>
2022-04-12 03:04:30 +1000

    map function

---
+++ green-space/Controllers/MapsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace green_space.Controllers
+{
+    public class MapsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
